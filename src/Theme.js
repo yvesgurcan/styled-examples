98cf11cd35202dad3418f997780aea2329621014
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-07-16 11:11:46 -0700

    theming

--- src/Theme.js
+++ src/Theme.js
@@ -1,8 +1,113 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import React, { Component, Fragment } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 
-export default () => (
-    <Fragment>
-        <p>I use the styled-components library!</p>
-    </Fragment>
-);
+import { SecondaryButton } from './ExtendingStyles';
+
+// define your theme objects
+export const DEFAULT_THEME = {
+    name: 'navy theme',
+    background: 'steelblue',
+    linkColor: 'lightblue',
+    codeBlockBackground: 'lightgrey'
+};
+
+export const SECONDARY_THEME = {
+    name: 'summer theme',
+    background: 'orange',
+    linkColor: 'yellow',
+    codeBlockBackground: 'gold'
+};
+
+export const Wrapper = styled.div`
+    padding: 20px;
+    background: ${props => props.theme.background};
+    margin-bottom: 50px;
+`;
+
+export const Link = styled.a`
+    color: ${props => props.theme.linkColor};
+`;
+
+export const FakeNav = styled.nav`
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: 10px;
+    border-bottom: 2px solid black;
+    justify-content: space-around;
+`;
+
+export const FakeNavItem = styled(Link)`
+    margin: 10px;
+`;
+
+export const Content = styled.section``;
+
+export const CodeBlock = styled.pre`
+    background: ${props => props.theme.codeBlockBackground};
+    overflow: scroll;
+    font-size: 16px;
+`;
+
+export default class Theming extends Component {
+    state = { currentTheme: DEFAULT_THEME };
+
+    changeTheme = () => {
+        if (this.state.currentTheme.name === DEFAULT_THEME.name) {
+            this.setState({ currentTheme: SECONDARY_THEME });
+        } else {
+            this.setState({ currentTheme: DEFAULT_THEME });
+        }
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <span>Current theme:</span>{' '}
+                    <b>{this.state.currentTheme.name}</b>
+                </div>
+                <SecondaryButton onClick={this.changeTheme}>
+                    Switch style
+                </SecondaryButton>
+                <ThemeProvider theme={this.state.currentTheme}>
+                    <Wrapper>
+                        <FakeNav>
+                            <FakeNavItem>Home</FakeNavItem>
+                            <FakeNavItem>Pricing</FakeNavItem>
+                            <FakeNavItem>About</FakeNavItem>
+                            <FakeNavItem>Contact</FakeNavItem>
+                        </FakeNav>
+                        <Content>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit, sed do eiusmod tempor{' '}
+                                <Link href="">
+                                    incididunt ut labore et dolore magna
+                                </Link>{' '}
+                                aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex
+                                ea commodo consequat.
+                            </p>
+                            <p>A block of code:</p>
+                            <CodeBlock>{`
+if (!rowIsEmpty) {
+    for (let j = 0; j < NUMBER_OF_COLUMNS; j++) {
+        const horizontalWinner = connectedHorizontally(j, row);
+            return horizontalWinner;
+    }
+}
+                    `}</CodeBlock>
+                            <p>
+                                Duis aute irure dolor in reprehenderit in
+                                voluptate velit esse cillum dolore eu fugiat
+                                nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                            </p>
+                        </Content>
+                    </Wrapper>
+                </ThemeProvider>
+            </Fragment>
+        );
+    }
+}
