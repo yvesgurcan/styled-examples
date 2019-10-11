4591e3ad53445bdade16692c83b160223b617fc3
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-11 14:42:37 -0700

    add info

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,14 @@ import React, { Fragment } from 'react';
 
 export default () => (
     <Fragment>
-        <div>Hello! Click on the links below to see examples of styled components.</div>
-        <br />
         <div>Bonjour! My name is Yves Gurcan.</div>
         <div>twitter: <a href="https://twitter.com/yvesgurcan">@yvesgurcan</a></div>
         <div>github: <a href="https://github.com/yvesgurcan">yvesgurcan</a></div>
         <div>linkedin: <a href="https://www.linkedin.com/in/yvesgurcan/">yvesgurcan</a></div>
+        <br />
+        <div>This demo: <a href="http://styled.yvesgurcan.com/">http://styled.yvesgurcan.com/</a></div>
+        <div>This repo: <a href="https://github.com/yvesgurcan/styled-examples">https://github.com/yvesgurcan/styled-examples</a></div>
+        <br />
+        <div>Hello! Click on the links below to see examples of styled components.</div>
     </Fragment>
 )
