07e2cf97646ea8e0b9d496933124711652b10209
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-11 10:43:21 -0700

    #followme

--- src/Home.js
+++ src/Home.js
@@ -1,2 +1,9 @@
-export default () =>
-  'Hello! Click on the links below to see examples of styled components.';
+import React, { Fragment } from 'react';
+
+export default () => (
+    <Fragment>
+        <div>Hello! Click on the links below to see examples of styled components.</div>
+        <br />
+        <div>Follow me on twitter: <a href="https://twitter.com/yvesgurcan">@yvesgurcan</a></div>
+    </Fragment>
+)
