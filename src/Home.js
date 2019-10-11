8552447aa7e63551671cc33023b1b6520dc39b12
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-10-11 12:34:27 -0700

    update contact stuff

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ export default () => (
     <Fragment>
         <div>Hello! Click on the links below to see examples of styled components.</div>
         <br />
-        <div>Follow me on twitter: <a href="https://twitter.com/yvesgurcan">@yvesgurcan</a></div>
+        <div>Bonjour! My name is Yves Gurcan.</div>
+        <div>twitter: <a href="https://twitter.com/yvesgurcan">@yvesgurcan</a></div>
+        <div>github: <a href="https://github.com/yvesgurcan">yvesgurcan</a></div>
+        <div>linkedin: <a href="https://www.linkedin.com/in/yvesgurcan/">yvesgurcan</a></div>
     </Fragment>
 )
